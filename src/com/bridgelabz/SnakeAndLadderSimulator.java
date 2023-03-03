ce0d6021a15d4b78d5e6786d79c8d4d77f2e86bc
drisyaathul <drisyak809@gmail.com>
2023-03-04 06:43:31 +0800

    Added: Player Checks for an Option using Switch Case

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -13,5 +13,19 @@ public class SnakeAndLadderSimulator {
         int die = random.nextInt(6)+1;
         System.out.println("Player1 rolls the die and the random die is  : "+die);
 
+        int option = random.nextInt(3)+1;
+        switch (option){
+            case 1:
+                System.out.println("No Play");
+                System.out.println("The Player stays in the same position: "+(singlePlayerPosition));
+                break;
+            case 2:
+                System.out.println("Ladder");
+                System.out.println("The Player moves ahead : "+(singlePlayerPosition+die));
+                break;
+            case 3:
+                System.out.println("Snake");
+                System.out.println("The Player moves behind : "+(singlePlayerPosition-die));
+        }
     }
 }
