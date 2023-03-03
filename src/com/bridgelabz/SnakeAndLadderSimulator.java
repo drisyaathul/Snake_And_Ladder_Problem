95aab4aeb70ebe63e93565a3abcd25f256aaf9ea
drisyaathul <drisyak809@gmail.com>
2023-03-03 21:08:50 +0800

    Added: Single Player Start Position is Zero

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
+        int singlePlayerPosition = 0;
+        System.out.println("Single Player at Start Position is Zero");
     }
 }
