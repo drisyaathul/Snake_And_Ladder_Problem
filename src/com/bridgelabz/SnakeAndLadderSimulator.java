0fd8db09240dbe6a293934c26f889cd62b4d4881
drisyaathul <drisyak809@gmail.com>
2023-03-03 22:42:50 +0800

    Added: Player1 Rolls the Die Using Random

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -1,9 +1,17 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadderSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         int singlePlayerPosition = 0;
         System.out.println("Single Player at Start Position is Zero");
+        System.out.println("GAME STARTS :-");
+
+        Random random = new Random();
+        int die = random.nextInt(6)+1;
+        System.out.println("Player1 rolls the die and the random die is  : "+die);
+
     }
 }
