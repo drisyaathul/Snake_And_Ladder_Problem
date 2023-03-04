ad0568313c9286d88e741207e929781f052bb47c
drisyaathul <drisyak809@gmail.com>
2023-03-04 12:24:40 +0800

    Added: Number of Times Dice Played

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -9,6 +9,7 @@ public class SnakeAndLadderSimulator {
         System.out.println("Welcome to Snake and Ladder Game");
 
         int singlePlayerPosition = 0;
+        int count = 0;;
         boolean position = true;
         System.out.println("Single Player at Start Position is Zero");
         System.out.println("GAME STARTS :-");
@@ -25,18 +26,16 @@ public class SnakeAndLadderSimulator {
                 switch (option) {
                     case 1:
                         System.out.println("No Play");
-                        System.out.println("The Player stays in the same position: " +(singlePlayerPosition));
+                        System.out.println("The Player stays in the same position: " +(singlePlayerPosition));//position after roll.
                         break;
                     case 2:
-
                         System.out.println("Ladder");
-                        System.out.println("The Player moves ahead to : " +(singlePlayerPosition+die));
+                        System.out.println("The Player moves ahead to : " +(singlePlayerPosition+die));//position after roll.
                         singlePlayerPosition = singlePlayerPosition + die;
                         break;
                     case 3:
-
                         System.out.println("Snake");
-                        System.out.println("The Player moves behind to : " +(singlePlayerPosition-die));
+                        System.out.println("The Player moves behind to : " +(singlePlayerPosition-die)); //position after roll.
                         singlePlayerPosition = singlePlayerPosition - die;
                         break;
                 }
@@ -47,9 +46,13 @@ public class SnakeAndLadderSimulator {
                     singlePlayerPosition = die;
                 }
             }
+            count++;
         }
         System.out.println("-------------------------------------------");
         System.out.println("*** The Player gets the Exact WINNING Position of 100 ***");
+        System.out.println("Number of times the Dice was played : " +count);
+
+
 
     }
 }
