a56e4d9a46b6a60d46f9f9cce19a2de4748e7437
drisyaathul <drisyak809@gmail.com>
2023-03-04 11:33:22 +0800

    Added: The Player reaches the Winning Position

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -1,31 +1,54 @@
 package com.bridgelabz;
-
 import java.util.Random;
 
 public class SnakeAndLadderSimulator {
+
+    static final int winningPosition = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
+
         int singlePlayerPosition = 0;
+        boolean position = true;
         System.out.println("Single Player at Start Position is Zero");
         System.out.println("GAME STARTS :-");
 
-        Random random = new Random();
-        int die = random.nextInt(6)+1;
-        System.out.println("Player1 rolls the die and the random die is  : "+die);
+        while (singlePlayerPosition != winningPosition) {
 
-        int option = random.nextInt(3)+1;
-        switch (option){
-            case 1:
-                System.out.println("No Play");
-                System.out.println("The Player stays in the same position: "+(singlePlayerPosition));
-                break;
-            case 2:
-                System.out.println("Ladder");
-                System.out.println("The Player moves ahead : "+(singlePlayerPosition+die));
-                break;
-            case 3:
-                System.out.println("Snake");
-                System.out.println("The Player moves behind : "+(singlePlayerPosition-die));
+            if (position == true) {
+                System.out.println("The Player1 current position is "+singlePlayerPosition);
+                Random random = new Random();
+                int die = random.nextInt(6) + 1;
+                System.out.println("Player1 rolls the die and the random die is  : " +die);
+
+                int option = random.nextInt(3) + 1;
+                switch (option) {
+                    case 1:
+
+                        System.out.println("No Play");
+                        System.out.println("The Player stays in the same position: " +(singlePlayerPosition));
+                        break;
+                    case 2:
+
+                        System.out.println("Ladder");
+                        System.out.println("The Player moves ahead to : " +(singlePlayerPosition+die));
+                        singlePlayerPosition = singlePlayerPosition + die;
+                        break;
+                    case 3:
+
+                        System.out.println("Snake");
+                        System.out.println("The Player moves behind to : " +(singlePlayerPosition-die));
+                        singlePlayerPosition = singlePlayerPosition - die;
+                        break;
+                }
+                System.out.println();
+                if (singlePlayerPosition<0 || singlePlayerPosition>winningPosition){
+                    singlePlayerPosition = 0;
+                }
+            }
         }
+        System.out.println("-------------------------------------------");
+        System.out.println("*** The Player reaches the WINNING Position ***");
+
     }
 }
